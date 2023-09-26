c90851f6af814a729866c233726141f84dcb9b8d
BOBO <bohdansheva06@gmail.com>
2023-09-26 22:53:22 +0300

    init

---
+++ src/container/nav/index.js
@@ -0,0 +1,47 @@
+class Nav {
+  static #HOME_PAGE = 'https://google.com'
+
+  static #back = () => {
+    return history.back()
+  }
+
+  static #forward = () => {
+    return history.forward()
+  }
+
+  static #reload = () => {
+    return location.reload()
+  }
+
+  static #channgePage = (href) => {
+    return location.assign(href)
+  }
+
+  static #go = () => {
+    try {
+      const url = new URL(window.input.value)
+      this.#channgePage(url.href)
+    } catch (e) {
+      alert('Write corect URL adress')
+      console.log(e)
+    }
+  }
+
+  static #home = () => {
+    return this.#channgePage(this.#HOME_PAGE)
+  }
+
+  static init = () => {
+    window.back.onclick = this.#back
+    window.forward.onclick = this.#forward
+    window.reload.onclick = this.#reload
+    window.home.onclick = this.#home
+    window.go.onclick = this.#go
+
+    window.input.value = location.href
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  Nav.init()
+})
